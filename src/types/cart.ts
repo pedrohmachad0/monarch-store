07384022843e58e149e1e07f0dd03860dcda367c
pedrohmachad0 <pedro.h.m.santos1909@gmail.com>
2026-08-28 16:21:08 -0300

    chore: add cart types

---
+++ src/types/cart.ts
@@ -0,0 +1,12 @@
+import type { Product } from './product';
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+  size?: string;
+  color?: string;
+}
+
+export interface CartState {
+  items: CartItem[];
+}
