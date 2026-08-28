5bf0bd88ca4577752f08a20d99524e7193c47859
pedrohmachad0 <pedro.h.m.santos1909@gmail.com>
2026-08-28 16:21:56 -0300

    chore: add application routes

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from '../pages/HomePage';
+import { CatalogPage } from '../pages/CatalogPage';
+import { ProductPage } from '../pages/ProductPage';
+import { CartPage } from '../pages/CartPage';
+import { CheckoutPage } from '../pages/CheckoutPage';
+import { LoginPage } from '../pages/LoginPage';
+import { AccountPage } from '../pages/AccountPage';
+
+export function AppRoutes() {
+  return <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/catalogo" element={<CatalogPage />} />
+    <Route path="/categoria/:category" element={<CatalogPage />} />
+    <Route path="/produto/:slug" element={<ProductPage />} />
+    <Route path="/carrinho" element={<CartPage />} />
+    <Route path="/checkout" element={<CheckoutPage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/conta" element={<AccountPage />} />
+  </Routes>;
+}
